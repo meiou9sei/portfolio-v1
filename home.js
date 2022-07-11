11599df6dc4c11fa7256ee71b629804de080680b
Cameron Avvampato <87138078+meiou9sei@users.noreply.github.com>
2022-07-11 09:53:32 -0400

    fix #about scroll behind header issue

--- home.js
+++ home.js
@@ -1,5 +1,4 @@
 document.querySelectorAll(".about-nav").forEach(e => {e.addEventListener('click', function(e) {
-    console.log('hi');
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
         behavior: 'smooth'
